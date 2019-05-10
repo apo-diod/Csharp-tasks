52cb46f0d045c4327503c7e5ea16789b62736aea
Andrew Podimov <podimov.andrey.odessa@gmail.com>
2019-05-10 09:34:26 +0300

    Add files via upload

---
+++ Theme3/3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Entry_Exams
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во бюджетных мест:");
+            int countOfBudget = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите кол-во контрактных мест:");
+            int countOfContract = int.Parse(Console.ReadLine());
+            Faculty faculty = new Faculty("Applied science", countOfBudget, countOfContract);
+            int i = 1;
+            while (faculty.CanAddToBudget)
+            {
+                faculty.addToBudget(new Entrant(i + "", i % 2 != 0));
+                i++;
+            }
+            Console.WriteLine("Бюджетные места кончились!");
+            while (faculty.CanAddToContract)
+            {
+                if (i % 2 != 0)
+                    faculty.addToContract(new Entrant(i + "", i % 2 != 0));
+                i++;
+            }
+            Console.WriteLine("Контрактные места кончились");
+            Console.WriteLine("Набор учеников закончился");
+        }
+    }
+}
