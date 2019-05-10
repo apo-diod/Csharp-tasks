c8a33b46d85fc4980c65804dd3cac446f5d50864
Andrew Podimov <podimov.andrey.odessa@gmail.com>
2019-05-10 09:27:08 +0300

    Add files via upload

---
+++ Theme3/1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Classes_basic_solution
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Equal function = new Equal(1, 0);
+            Console.WriteLine(function.Func(10));
+            Console.WriteLine(function.Root());
+        }
+    }
+}
