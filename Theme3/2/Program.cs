648b363d4aed576004531f31bdea1c808a655ee9
Andrew Podimov <podimov.andrey.odessa@gmail.com>
2019-05-10 09:29:39 +0300

    Add files via upload

---
+++ Theme3/2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Classes_overload_soution
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Sequence seq1 = new Sequence(10, 1, 2);
+            seq1.Print();
+            Console.WriteLine(seq1.CountOfPositive);
+            Console.WriteLine(seq1.MultBetw());
+            Sequence seq2 = new Sequence(Console.ReadLine());
+            seq2.Print();
+            Console.WriteLine(seq2.CountOfPositive);
+            Console.WriteLine(seq2.MultBetw());
+        }
+    }
+}
