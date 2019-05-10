554d3cec61a36fb49bfa28cfd0ed7707bbf7f9f2
Andrew Podimov <podimov.andrey.odessa@gmail.com>
2019-05-10 09:30:59 +0300

    Add files via upload

---
+++ Theme3/3/Entrant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Entry_Exams
+{
+    public class Entrant
+    {
+        public readonly string Name;
+        public readonly bool isContract;
+        public Entrant(string name, bool contr)
+        {
+            Name = name;
+            isContract = contr;
+        }
+    }
+}
