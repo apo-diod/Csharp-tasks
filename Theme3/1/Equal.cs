0af7003cb6a1ea5f80d476afac1936255468e98f
Andrew Podimov <podimov.andrey.odessa@gmail.com>
2019-05-10 09:26:52 +0300

    Add files via upload

---
+++ Theme3/1/Equal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Classes_basic_solution
+{
+    public class Equal
+    {
+        private double a;
+
+        private double b;
+
+        public double A
+        {
+            get
+            {
+                return a;
+            }
+        }
+
+        public double B
+        {
+            get
+            {
+                return b;
+            }
+        }
+
+        public Equal(double A, double B)
+        {
+            this.a = A;
+            this.b = B;
+        }
+
+        public double Func(double x)
+        {
+            return A * x + B;
+        }
+
+        public (bool, double) Root()
+        {
+            bool v = Math.Abs(A) > 0 ;
+            return v ? (true, (-B) / A) : (false, 0);
+        }
+    }
+}
